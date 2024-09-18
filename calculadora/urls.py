a7c78b0bbf9afdf86f0811a56d7eed9324c7848c
Rafael Alves Pessoa <rafael.alvesp@outlook.com>
2024-09-18 14:39:37 -0300

    novas-urls

--- calculadora/urls.py
+++ calculadora/urls.py
@@ -15,16 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-from calculator.views import home, contato, sobre
-
-def my_view(request):
-    return HttpResponse('Uma linda String')
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', home),
-    path('contato/', contato ),
-    path('sobre/', sobre),
+    path('', include('calculator.urls'))
+
 ]
